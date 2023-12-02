4d544c297b348f15aca570b9e4160d9fe5080e8b
Kris Z <krisz@KrisdeMacBook-Air.local>
2023-12-03 00:16:24 +1030

    add project again

---
+++ MyFirstGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 50f;
+    public float sideForce = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World!");
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce*Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideForce*Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideForce*Time.deltaTime, 0, 0,ForceMode.VelocityChange);
+        }
+    }
+}
